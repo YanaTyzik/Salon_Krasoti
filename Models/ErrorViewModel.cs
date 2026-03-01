c1f276bb3d16ebf371d4e7460a5cfcb2eb47b046
Kseniapav <kseniapavlova5916@gmail.com>
2026-03-02 01:07:47 +0300

    пофиксила что могла

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace LisBlanc.AdminPanel.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
